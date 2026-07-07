2711cc9937a08e5fcf2c0baee3aedc915e75addd
kholmetskii <150633190+kholmetskii@users.noreply.github.com>
2026-07-07 05:50:35 +0100

    Remove hspec-discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,10 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Test.Hspec
+
+import qualified Dtmc.StochasticSpec
+
+main :: IO ()
+main =
+    hspec $ do
+        Dtmc.StochasticSpec.spec
